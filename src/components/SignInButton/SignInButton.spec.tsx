c450d8d67441cd3bc72ac4704f8e73d75687fa7c
blackbook <blackbook@Mac-mini-de-blackbook.local>
2021-06-21 22:34:41 -0300

    test flow implementation

---
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import { mocked } from 'ts-jest/utils'
+import { useSession } from 'next-auth/client';
+import { SingInButton } from '.'
+
+jest.mock('next-auth/client')
+
+describe('ActiveLink component', () => {
+  it('renders correctly when user is not signedin', () => {
+    const useSessionMocked = mocked(useSession)
+
+    useSessionMocked.mockReturnValueOnce([null, false])
+
+    render(
+      <SingInButton />
+  )
+  
+  expect(screen.getByText('Sign in with Github')).toBeInTheDocument()
+  })
+
+  it('renders correctly when user is signedin', () => {
+    const useSessionMocked = mocked(useSession)
+
+    useSessionMocked.mockReturnValue([
+      { user: {name: 'John Doe', email: 'john.doe@example.com'}, expires: 'fake-expires' }, 
+      false
+    ])
+
+
+    render(
+      <SingInButton />
+  )
+  
+  expect(screen.getByText('John Doe')).toBeInTheDocument()
+  })
+})
